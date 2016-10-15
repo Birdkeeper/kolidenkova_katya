d656b11f30ae26515adecee1406514d99606a0fe
Kirill Malyshev <kirill.malyshe@gmail.com>
2016-10-15 16:30:40 +1000

    3 задание

--- homeworks1/1__3__Expression/main.py
+++ homeworks1/1__3__Expression/main.py
@@ -16,7 +16,6 @@ while i != len(string):
     if re.match(r'(-?\d+((\.)|(,))?\d*?[/+\-*]?)+', string[i:]) != None:
         word = re.match(r'(-?\d+((\.)|(,))?\d*[/+\-*]?)+', string[i: ]).group()
         words.append(word)
-        #print(word)
         i += int(re.match(r'(-?\d+((\.)|(,))?\d*[/+\-*]?)+', string[i: ]).end())
     else:
         i += 1
@@ -29,7 +28,25 @@ for i in range(0, len(words)):
 for str in words:
     print(str, " = ", eval(str))
 
+"""
+Модули в коде!
+
+Докстринги
+
+циклы по строкам пиши с помошью for symbol in string, а не while цикла =)
+
+Строки для тестирования лучше принимать в качестве аргументов командной строки
+(argparse библиотека), а не через input().
+Так удобней, не надо строку каждый раз вводить руками
 
 
 
+python main.py
+input string
+84*54 фыва 99-65 54/3
+
+84*54  =  4536
+99-6554/3  =  -2085.6666666666665 - Это явно неверный результат
+
+"""
 
